e24760a4801658e624c7e2bc910e6e0263201324
Sayan Biswas <sayan@pokurt.me>
2021-10-28 15:33:53 +0530

    ScanSource: Add invoked by and banned by differently
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- sibyl/database/scan.go
+++ sibyl/database/scan.go
@@ -2,26 +2,30 @@ package database
 
 import "time"
 
+type Scan ScanSource
+
 type ScanSource struct {
-	Id       uint64    `json:"id" gorm:"primaryKey"`
-	UserID   int64     `json:"user_id"`
-	Reason   string    `json:"reason"`
-	Banned   bool      `json:"banned"`
-	Date     time.Time `json:"date"`
-	Message  string    `json:"message"`
-	BannedBy int64     `json:"banned_by"`
+	Id        uint64    `json:"id" gorm:"primaryKey"`
+	InvokedBy int64     `json:"invoked_by"`
+	UserID    int64     `json:"user_id"`
+	Reason    string    `json:"reason"`
+	Banned    bool      `json:"banned"`
+	Date      time.Time `json:"date"`
+	Message   string    `json:"message"`
+	BannedBy  int64     `json:"banned_by"`
 }
 
-func AddScan(UserId int64, Reason string, Message string, BannedBy int64) {
+func AddScan(UserId int64, Reason string, Message string, invokedVy int64) {
 	tx := SESSION.Begin()
-	ss := &ScanSource{UserID: UserId, Reason: Reason, Message: Message, Date: time.Now(), BannedBy: BannedBy}
+	ss := &ScanSource{UserID: UserId, Reason: Reason, Message: Message, Date: time.Now(), InvokedBy: invokedVy}
 	tx.Save(ss)
 	tx.Commit()
 }
 
-func (s *ScanSource) Approve() {
+func (s *ScanSource) Approve(by int64) {
 	tx := SESSION.Begin()
-	tx.Model(ScanSource{}).Update("banned", true)
+	tx.Model(ScanSource{}).Update("banned", true).Update("banned_by", by)
+	tx.Commit()
 }
 
 func (s *ScanSource) Reject() {
