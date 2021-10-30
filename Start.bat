f6d94977e88254a48851e91e8f963fdad9d0a0f7
TsunayoshiSawada <animekaizokux@gmail.com>
2021-10-30 19:42:53 +0530

    Edits to start bat

--- Start.bat
+++ Start.bat
@@ -1,11 +1,6 @@
 @echo off
-del pull_log.txt /F
-del build_log.txt /F
 TITLE PsychoPass
 echo.
-nssm stop PsychoPass
-git pull > pull_log.txt
-go build > build_log.txt
-cls
-nssm start PsychoPass
+:: stop service, delete existing pull and build log files, git pull, build, then start service again. 
+nssm stop PsychoPass && del pull_log.txt /F && del build_log.txt /F && git pull > pull_log.txt && go build > build_log.txt && nssm start PsychoPass && echo All Done, check once. 
 :: Hail Hydra
