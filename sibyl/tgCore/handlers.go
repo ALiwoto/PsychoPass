3344ba2e4ff5f2ab3acb0625bcf0f7be016f5e51
aliwoto <aminnimaj@gmail.com>
2021-10-30 06:35:33 -0400

    Send report sending problem
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- sibyl/tgCore/handlers.go
+++ sibyl/tgCore/handlers.go
@@ -2,12 +2,13 @@ package tgCore
 
 import (
 	"github.com/AnimeKaizoku/PsychoPass/sibyl/tgCore/plugins/infoPlugin"
+	"github.com/AnimeKaizoku/PsychoPass/sibyl/tgCore/plugins/reportPlugin"
 	"github.com/AnimeKaizoku/PsychoPass/sibyl/tgCore/plugins/tokenPlugin"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
 func LoadAllHandlers(d *ext.Dispatcher, triggers []rune) {
 	infoPlugin.LoadAllHandlers(d, triggers)
-	//reportPlugin.LoadAllHandlers(d, triggers)
+	reportPlugin.LoadAllHandlers(d, triggers)
 	tokenPlugin.LoadAllHandlers(d, triggers)
 }
--- sibyl/tgCore/plugins/reportPlugin/handlers.go
+++ sibyl/tgCore/plugins/reportPlugin/handlers.go
@@ -4,6 +4,7 @@ import (
 	"github.com/AnimeKaizoku/PsychoPass/sibyl/core/sibylConfig"
 	sv "github.com/AnimeKaizoku/PsychoPass/sibyl/core/sibylValues"
 	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
 func SendReportHandler(r *sv.Report) {
@@ -33,4 +34,8 @@ func SendReportHandler(r *sv.Report) {
 	for _, chat := range bases {
 		sendReportMessage(chat, text, opts)
 	}
+}
+
+func LoadAllHandlers(d *ext.Dispatcher, triggers []rune) {
+	sv.SendReportHandler = SendReportHandler
 }
