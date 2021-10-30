e43b759d10210d34e87049f80f61f3bc7c900d64
TsunayoshiSawada <animekaizokux@gmail.com>
2021-10-30 19:50:10 +0530

    Removed start bat

--- Start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-TITLE PsychoPass
-echo.
-:: stop service, delete existing pull and build log files, git pull, build, then start service again. 
-nssm stop PsychoPass && del pull_log.txt /F && del build_log.txt /F && git pull > pull_log.txt && go build > build_log.txt && nssm start PsychoPass && echo All Done, check once. 
-:: Hail Hydra
