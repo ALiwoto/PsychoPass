a9644b3b5b77b895452c9a843aefa9c10b92f16b
aliwoto <aminnimaj@gmail.com>
2021-11-04 09:12:10 -0400

    Ignore stderr and try to parse stdout anyway in GetGitStats function
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- sibyl/core/utils/shellUtils/helpers.go
+++ sibyl/core/utils/shellUtils/helpers.go
@@ -24,8 +24,8 @@ func Shellout(command string) (string, string, error) {
 // GetGitStats function will return the git stats in the following format:
 // "d8e6e45 \n d8e6e45d52f7bf164a995e22abb81ffc6e3eeae1 \n 3 0"
 func GetGitStats() string {
-	stdout, stderr, err := Shellout(gitCmd)
-	if err != nil || len(stderr) != 0 {
+	stdout, _, err := Shellout(gitCmd)
+	if err != nil || len(stdout) == 0 {
 		return ""
 	}
 
