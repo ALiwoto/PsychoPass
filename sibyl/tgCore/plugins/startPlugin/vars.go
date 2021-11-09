1202a0a13102aa2ab4fb5fad586c93b7339059e0
aliwoto <aminnimaj@gmail.com>
2021-11-09 16:55:29 -0500

    Add vars.go file
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

---
+++ sibyl/tgCore/plugins/startPlugin/vars.go
@@ -0,0 +1,8 @@
+package startPlugin
+
+import "github.com/PaulSonOfLars/gotgbot/v2"
+
+var (
+	normalButtonsRows [][]gotgbot.InlineKeyboardButton
+	singleButtonsRows [][]gotgbot.InlineKeyboardButton
+)
