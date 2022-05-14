3a6a469da110c6d57f3a78fbb756fc575a5c2831
aliwoto <woto@kaizoku.cyou>
2022-05-14 05:58:17 +0200

    Add comment headers to all files.
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

--- sibyl/entryPoints/pollingHandlers/handlers.go
+++ sibyl/entryPoints/pollingHandlers/handlers.go
@@ -1,1 +1,5 @@
+/*
+ * This file is part of PsychoPass Project (https://github.com/MinistryOfWelfare/PsychoPass).
+ * Copyright (c) 2021-2022 PsychoPass Authors, Ministry of welfare.
+ */
 package pollingHandlers
--- sibyl/entryPoints/tokenHandlers/handlers.go
+++ sibyl/entryPoints/tokenHandlers/handlers.go
@@ -1,3 +1,7 @@
+/*
+ * This file is part of PsychoPass Project (https://github.com/MinistryOfWelfare/PsychoPass).
+ * Copyright (c) 2021-2022 PsychoPass Authors, Ministry of welfare.
+ */
 package tokenHandlers
 
 import (
