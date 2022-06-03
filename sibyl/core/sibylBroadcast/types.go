a52d48b263d4f004a0ba5494e1592a4634b9a055
aliwoto <woto@kaizoku.cyou>
2022-06-04 01:14:08 +0200

    Add new package: sibylBroadcast
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

---
+++ sibyl/core/sibylBroadcast/types.go
@@ -0,0 +1,6 @@
+/*
+ * This file is part of PsychoPass Project (https://github.com/MinistryOfWelfare/PsychoPass).
+ * Copyright (c) 2021-2022 PsychoPass Authors, Ministry of welfare.
+ */
+
+package sibylBroadcast
